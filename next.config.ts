abee69ada3c8aeda325091ad226488246b2fffe2
刘雨江 <liuyujiang@liuyujiangdeMacBook-Air.local>
2026-07-03 23:55:49 +0700

    fix: use proxyClientMaxBodySize instead of serverActions.bodySizeLimit

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   serverExternalPackages: ['better-sqlite3'],
   experimental: {
-    serverActions: {
-        bodySizeLimit: '50mb',
-    },
+    proxyClientMaxBodySize: '50mb',
   },
 };
 
