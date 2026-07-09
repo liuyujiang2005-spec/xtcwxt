50a537dbd7b05d98369035ac27579c99170575d9
liuyujiang2005-spec <liuyujiang@users.noreply.github.com>
2026-07-09 17:13:04 +0000

    chore: 清理临时脚本和测试文件

--- fix_select.js
+++
@@ -1,7 +0,0 @@
-const fs = require("fs");
-["src/app/(main)/revenue/NewIncomeDialog.tsx", "src/app/(main)/revenue/EditIncomeDialog.tsx", "src/app/(main)/expenses/NewExpenseDialog.tsx"].forEach(fp => {
-  let s = fs.readFileSync(fp, "utf8");
-  s = s.replace(/onValueChange=\{setCurrency\}/g, "onValueChange={v => setCurrency(v || 'CNY')}");
-  fs.writeFileSync(fp, s);
-  console.log(fp, "fixed");
-});
--- start_parser.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd /root/xtcwxt
-set -a
-source .env
-set +a
-source venv/bin/activate
-exec python3 table_parser.py --serve --port 8800
